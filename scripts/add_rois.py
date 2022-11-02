fe506ee03a7d627486f7a090633f01755c1caea5
Dominik Lindner <d.lindner@dundee.ac.uk>
2022-11-02 16:40:51 +0000

    Fix roi script

--- scripts/add_rois.py
+++ scripts/add_rois.py
@@ -1,22 +1,32 @@
 import re
 
+import numpy as np
 import omero
+import omero.cli
 from omero.rtypes import rstring
 from omero_rois import (masks_from_label_image, mask_from_binary_image)
 from skimage.io import imread
 
 
-RGBA_MASKS = (128, 128, 128, 180)
-RGBA_ROI = (128, 0, 128, 180)
+RGBA_MASKS = (255, 255, 0, 200)
+RGBA_ROI = (255, 0, 255, 200)
 
 def get_paths():
     # /uod/idr/filesets/idr0144-baskay-jawbone/20221019-Globus/idr0000-baskay-3drec/experimentE/PROCESSED/masks/E_22.png
     pattern = re.compile(r".+/(?P<image_type>.+)/(?P<image_name>.+)\.png")
     with open("roi_files.txt", mode='r') as input_file:
         for line in input_file.readlines():
+            line = line.strip()
             m = pattern.match(line.strip())
             if m:
                 yield (m.group("image_type"), m.group("image_name"), line)
+
+
+def get_images(conn):
+    project = conn.getObject("Project", attributes={"name": "idr0144-baskay-jawbone/experimentA"})
+    for dataset in project.listChildren():
+        for image in dataset.listChildren():
+            yield image
 
 
 def get_image(conn, image_name):
@@ -30,20 +40,21 @@ def get_image(conn, image_name):
 
 
 def create_rois(path, type):
-    roi_img = imread(path)
+    roi_img = imread(path, as_gray=True)
+    roi_img = roi_img.astype(int)
     rois = []
     if type == "masks":
-        masks = masks_from_label_image(roi_img, rgba=RGBA_MASKS, raise_on_no_mask=False)
-        for i, mask in enumerate(masks):
+        labels = np.unique(roi_img)
+        for n in range(1, len(labels)):
+            mask = mask_from_binary_image(roi_img == labels[n], rgba=RGBA_MASKS)
             roi = omero.model.RoiI()
-            roi.setName(rstring(i))
+            mask.setTextValue(rstring(n))
             roi.addShape(mask)
             rois.append(roi)
     elif type == "roi":
-        mask = mask_from_binary_image(roi_img, rgba=RGBA_ROI, raise_on_no_mask=False)
+        mask = mask_from_binary_image(roi_img, rgba=RGBA_ROI, text="ROI", raise_on_no_mask=False)
         if mask:
             roi = omero.model.RoiI()
-            roi.setName(rstring("ROI"))
             roi.addShape(mask)
             rois.append(roi)
     else:
@@ -60,9 +71,24 @@ def save_rois(conn, im, rois):
         us.saveAndReturnObject(roi)
 
 
+def delete_rois(conn, im):
+    result = conn.getRoiService().findByImage(im.id, None)
+    to_delete = []
+    for roi in result.rois:
+        to_delete.append(roi.getId().getValue())
+    if to_delete:
+        print(f"Deleting existing {len(to_delete)} rois on image {im.name}.")
+        conn.deleteObjects("Roi", to_delete, deleteChildren=True, wait=True)
+
+
 with omero.cli.cli_login() as c:
     conn = omero.gateway.BlitzGateway(client_obj=c.get_client())
+
+    for img in get_images(conn):
+        delete_rois(conn, img)
+
     for type, image, path in get_paths():
+        print(path)
         rois = create_rois(path, type)
         img = get_image(conn, image)
         save_rois(conn, img, rois)
